4cee2eb9d72f7b1ad65dfce291a762fa87033ccc
Python Learning 11-01-23 <rickeyem2@gmail.com>
2023-11-14 14:12:21 -0800

    quick square feet program

---
+++ square_foot.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def square_feet():
+    length = st.session_state['feet']
+    width = st.session_state['width']
+    results = length * width
+    return results
+
+
+st.text('Get Square Feet')
+st.number_input('Enter Length', key='feet', step=1)
+st.number_input('Enter width:', key='width', step=1)
+
+total = square_feet()
+display = st.write(f'{total}sq')
+st.button('Results', display)
+
